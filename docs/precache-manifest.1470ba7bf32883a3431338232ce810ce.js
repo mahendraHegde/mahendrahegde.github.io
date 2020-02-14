57a45841dc88f4151bf46c46c0db3189f7a30c31
Mahendra <hegdemahendra9@gmail.com>
2020-02-14 16:46:21 +0530

    added docs

---
+++ docs/precache-manifest.1470ba7bf32883a3431338232ce810ce.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "449e3514612d84144710",
+    "url": "/static/js/main.449e3514.chunk.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "8c25e09602aacc1fd210c14120e3527f",
+    "url": "/static/media/me2.8c25e096.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "729ae9f157eba1a30647",
+    "url": "/static/js/2.729ae9f1.chunk.js"
+  },
+  {
+    "revision": "729ae9f157eba1a30647",
+    "url": "/static/css/2.cec51052.chunk.css"
+  },
+  {
+    "revision": "812245a13d825592f902eb7b99c1cf92",
+    "url": "/index.html"
+  }
+];
