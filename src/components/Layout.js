6739ac4ea61ddd2fc68d2bc795beb4671126457d
Mahendra <hegdemahendra9@gmail.com>
2021-01-04 21:38:12 +0530

    add bio part 1

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,27 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { FaFacebook, FaLinkedinIn, FaGithub, FaCircle } from "react-icons/fa";
-import { SiGmail } from "react-icons/si";
-import Utils from "../utils/index";
-import { Link } from "react-router-dom";
-import { Button } from "react-bootstrap";
+import React from "react";
+import {  Container } from "react-bootstrap";
 import Sidebar from "./leftbar";
 
-const {
-  MENU_ITEMS,
-  SOCIAL_MEDIA,
-  THEME: { color },
-} = Utils;
-const SOCIAL_ICON_SIZE = 22;
-let prev = 1;
 function Layout(props) {
-  const [selectedMenu, setSelectedMenu] = useState(0);
-  useEffect(() => {
-    console.log(props);
-    const loc = window.location.href.split("/");
-    setSelectedMenu(loc[loc.length - 1]);
-  }, [window.location.href]);
   return <>
   <Sidebar/>
+  <Container className="p-3 p-lg-5 theme-bg-light"> {props.children}</Container>
   </>;
 }
 
--- src/utils/index.js
+++ src/utils/index.js
@@ -15,8 +15,15 @@ const THEME = {
     primary: "#36cd61",
   },
 };
+const DETAILS={
+  name:"Mahendra Hegde",
+  designation:"Software Engineer",
+  shrotAbout:`Hi, my name is Mahendra Hegde and I'm a Software Engineer. Welcome to my personal website!`,
+  about:`I'm a software engineer specialised in frontend and backend development for complex scalable web apps. Would like to know how I may help your project? Check out my project portfolio and online resume.`
+}
 export default {
   MENU_ITEMS,
   SOCIAL_MEDIA,
   THEME,
+  DETAILS
 };
