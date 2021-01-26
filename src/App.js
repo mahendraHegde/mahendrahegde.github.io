e00453c3cb196a8f5d16c6a99fd03655085df463
Mahendra <hegdemahendra9@gmail.com>
2021-01-26 19:18:28 +0530

    integrated api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
+import { ApolloProvider } from "@apollo/client";
 import { Switch, Route } from "react-router-dom";
 import { MENU_ITEMS } from "./utils/dom-utils";
+import { client } from "./utils/api/graphql/client";
 import "./styles/index.scss";
 import Layout from "./components/Layout";
 function App() {
   return (
-    <Layout>
-      <Switch>
-        {Object.values(MENU_ITEMS).map(({ link, component }) => (
-          <Route key={link} exact path={link} component={component} />
-        ))}
-      </Switch>
-    </Layout>
+    <ApolloProvider client={client}>
+      <Layout>
+        <Switch>
+          {Object.values(MENU_ITEMS).map(({ link, component }) => (
+            <Route key={link} exact path={link} component={component} />
+          ))}
+        </Switch>
+      </Layout>
+    </ApolloProvider>
   );
 }
 
