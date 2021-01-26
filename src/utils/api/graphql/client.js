10a2b248082376afdf51bb1317adce9918dd65b0
Mahendra <hegdemahendra9@gmail.com>
2021-01-26 19:31:22 +0530

    [grapql-client]update server address

--- src/utils/api/graphql/client.js
+++ src/utils/api/graphql/client.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "http://localhost:3000/query",
+  uri: "https://mahi-notification-service.herokuapp.com/query",
   cache: new InMemoryCache(),
 });
