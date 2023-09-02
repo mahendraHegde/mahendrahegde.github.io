136a01afb03b00d827efbfb15dd57f8984b4c969
Mahendra <hegdemahendra9@gmail.com>
2023-09-02 19:52:52 -0400

    remove default values from calender questions

--- src/views/contact.jsx
+++ src/views/contact.jsx
@@ -184,8 +184,6 @@ export default function Contact() {
           style={{ width: "100%", height: "100%", overflow: "scroll" }}
           config={{
             layout: "month_view",
-            name: "your name?",
-            email: "your email?",
           }}
         />
       </Container>
